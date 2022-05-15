3a14503e37df7a7686fe1131cefbb0e89c24ae61
Amieva, Abel Aron <abelamieva@gmail.com>
2022-05-15 13:12:43 -0300

    docs actualizado - url portafolio actualizado

--- src/components/practices/info.ts
+++ src/components/practices/info.ts
@@ -122,6 +122,7 @@ export const infoEspañol = [
         name: 'Este Portfolio-App',
         img: imgPortfolioApp,
         desc: "Acerca de este portfolio, fue creado en React con el template de Typescript, usamos Redux para mantener el estado de la aplicación, los switch, botones, y modal del email son de NextUI, es responsive usando los breakpoints de Bootstrap, tambien cree el serviceWorker con Workbox, utilizando su BackgroundSinc para poder enviar el email de contacto aunque no tengas conexión a internet",
-        git: 'https://github.com/abel1711/appConsolaClimaNODE'
+        url: 'https://bit.ly/portafolio-abel-amieva',
+        git: 'https://bit.ly/github-portafolio-abel-amieva'
     },
 ]
